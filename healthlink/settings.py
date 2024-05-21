53f4d0b159d303781dd862b87d4e38095216717c
Hanen Wechteti <hanenwechteti27@gmail.com>
2024-05-21 16:12:45 +0200

    migration

--- healthlink/settings.py
+++ healthlink/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'healthlink.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'healthlink',
+        'USER': 'postgres',
+        'PASSWORD': 'adam2022',
+        'Host': 'localhost'
     }
 }
 
@@ -118,7 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'healthlink/static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from datetime import datetime
+from nurses.models import Nurse
 
-# Create your models here.
+class Task(models.Model):
+    nurse = models.ForeignKey(Nurse, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=200)
+    adress = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    zipcode = models.CharField(max_length=200)
+    description = models.CharField(blank=True)
+    dispo = models.IntegerField()
+    hygiene = models.IntegerField()
+    monitoring =  models.CharField(max_length=200)
+    medication =  models.CharField(max_length=200)
+    blood =  models.CharField(max_length=200)
+    photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    is_published = models.BooleanField(default=True)
+    list_date = models.DateTimeField(default=datetime.now, blank=True)
+    def __str__(self):
+        return self.title
